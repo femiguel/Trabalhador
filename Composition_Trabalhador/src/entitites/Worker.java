9f74be37ae21d49ff4848a1ea3d4037cdd2ad87c
Felipe <felipemiguelfutebol@hotmail.com>
2024-04-30 12:20:01 -0300

    Programa Finalizado

--- Composition_Trabalhador/src/entitites/Worker.java
+++ Composition_Trabalhador/src/entitites/Worker.java
@@ -80,12 +80,11 @@ public class Worker {
 		for (HourContract c : contracts) {
 			cal.setTime(c.getDate());
 			int c_year = cal.get(Calendar.YEAR);
-			int c_month = cal.get(Calendar.MONTH);
-
+			int c_month = 1 + cal.get(Calendar.MONTH);
 			if (year == c_year && month == c_month) {
 				sum += c.totalValue();
 			}
-
 		}
-	}g
+		return sum;
+	}
 }
